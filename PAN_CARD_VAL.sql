50631e90dc6b7f171c9b819940a83bf528e76817
Suraj Kanojiya <86044725+Suraj-Kanojiya@users.noreply.github.com>
2026-01-12 14:58:49 +0530

    Refactor PAN card validation SQL script

--- PAN_CARD_VAL.sql
+++ PAN_CARD_VAL.sql
@@ -1,5 +1,6 @@
 
---Invalid object name occurs when SQL Server cannot find the object in the current database or schema. Using the fully qualified name or switching the database resolves it
+--Invalid object name occurs when SQL Server cannot find the object in the current database or schema. 
+--Using the fully qualified name or mentioning the database resolves it
 USE PANCARD; 
 GO
 
@@ -8,7 +9,7 @@ Select * from PANNumberData;
 
 Alter table PANNumberData Alter Column Pan_numbers varchar(50);
 
---check for duplicates--
+--check for duplicates
 Select Pan_Numbers, count(1)
 from PANNumberData
 GROUP BY Pan_Numbers
@@ -28,7 +29,7 @@ WHERE Pan_numbers <> UPPER(Pan_numbers) COLLATE Latin1_General_CS_AS;
 --Cleaned PAN Number
 Select Distinct Pan_Numbers from  PANNumberData
 
---Cleaned pan numbers plus All letters in uppercase
+--Cleaned pan numbers + All letters in uppercase
 Select Distinct UPPER(Pan_numbers) from  PANNumberData
 
 --Function to check if the adjacent characters are same
@@ -48,7 +49,7 @@ BEGIN
 	return false; non of the adjac characters were same
 end;*/
 
-Create Function dbo.adj_char(@pstr Varchar(50))
+Create Function adjCharSame(@pstr Varchar(50))
 Returns BIT
 AS 
 Begin
@@ -57,31 +58,79 @@ Begin
 
    While @i<@len 
    Begin
-      if  SUBSTRING (@pstr, @i,1)=SUBSTRING(@pstr, @i+1,1)
-	    return 1; -- Adj characters same
+      if  SUBSTRING(@pstr, @i,1)=SUBSTRING(@pstr, @i+1,1)
+	  Begin
+		  return 1; -- Adj characters same
+      END;
 	  SET @i=@i+1;
-   END
+   END;
    Return 0; -- Adj Characters not same
-
 END;
+GO 
 
-Select dbo.adj_char('JJCHK4574O')
+
+--test for same adjacent characters
+Select adjCharSame('JACHK4574O')
 
 
-Create Function dbo.adj_charseq(@pstr Varchar(50))
-Returns BIT
-AS 
-Begin
-   Declare @i int=1;
-   Declare @len INT = LEN(@pstr);
+CREATE FUNCTION dbo.adjCharSEQ (@pstr VARCHAR(50))
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @i INT = 1;
+    DECLARE @len INT = LEN(@pstr);
 
-   While @i<@len 
-   Begin
-      if ASCII(SUBSTRING (@pstr, @i+1,1))-ASCII(SUBSTRING(@pstr, @i,1))=1
-	    return 1; -- characters in SEQ
-	  SET @i=@i+1;
-   END
-   Return 0; -- Characters not in seq
+    WHILE @i < @len
+    BEGIN
+        -- If any adjacent characters are NOT sequential
+        IF ASCII(SUBSTRING(@pstr, @i + 1, 1))
+           - ASCII(SUBSTRING(@pstr, @i, 1)) <> 1
+        BEGIN
+            RETURN 0;  -- NOT fully sequential
+        END;
 
+        SET @i = @i + 1;
+    END;
+
+    RETURN 1;  -- FULLY sequential
 END;
-Select dbo.adj_charseq('ACZSE2852I')
+GO
+--check for character sequence
+Select dbo.adjCharSEQ('ACZSE2856I');
+
+--Regular expression to validate the pattern of PAN Numbers
+Select * from PANNumberData
+where Pan_Numbers like '[A-Z][A-Z][A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9][A-Z]';
+
+
+create view validinvalid
+as 
+With cleanedPAN as (
+   Select DISTINCT UPPER(Pan_numbers) as  Pan_Numbers from  PANNumberData
+   ),
+    ValidPAN as
+    (Select * from cleanedPAN
+    where dbo.adjCharSame(Pan_Numbers)=0
+    AND dbo.adjCharSEQ(SUBSTRING(Pan_Numbers,1,5))=0
+	AND dbo.adjCharSEQ(SUBSTRING(Pan_Numbers,6,4))=0
+    AND Pan_Numbers like '[A-Z][A-Z][A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9][A-Z]')
+
+Select cln.Pan_numbers,
+case when vld.Pan_numbers is not NULL 
+then 'VALID PAN' 
+else 'INVALID PAN' 
+end as Status
+from CleanedPAN cln
+left join ValidPAN vld on cln.Pan_numbers=vld.Pan_numbers;
+
+With report as 
+	(SELECT
+    (Select count(*) from PANNumberData) as total_processed_records,
+    COUNT(CASE WHEN status = 'VALID PAN' THEN 1 END)   AS total_valid_pans,
+    COUNT(CASE WHEN status = 'INVALID PAN' THEN 1 END) AS total_invalid_pans
+	FROM validinvalid)
+Select total_processed_records,total_valid_pans,total_invalid_pans,
+(total_processed_records-(total_valid_pans+total_invalid_pans)) as total_missing_pans
+from report;
+
+
